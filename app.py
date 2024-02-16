587447b8643ab262f5194f0916f9e5b49dc5449b
Saad Azil <138615559+SaadAzil3@users.noreply.github.com>
2024-02-16 17:42:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask import Flask
 from routes import pages
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from functools import partial
 
 load_dotenv()
 
-def create_app():
+def create_app(environ, start_response):
     app = Flask(__name__)
     client = MongoClient(os.environ.get("MONGODB_URI"))
     app.db = client.get_default_database()
@@ -15,4 +16,7 @@ def create_app():
     
     # You can add additional setup code here if needed
     
-    return app
+    return app(environ, start_response)
+
+# If you're running this using Gunicorn, you can create a partial application
+application = partial(create_app)
